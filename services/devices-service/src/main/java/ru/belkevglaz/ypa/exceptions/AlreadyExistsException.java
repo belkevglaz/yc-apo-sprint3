7038553e325550240a225f74c94835087aa78fea
Ivan Aksenov <belkevglaz@gmail.com>
2024-09-17 21:15:54 +0300

    Optimized imports. #2

--- services/devices-service/src/main/java/ru/belkevglaz/ypa/exceptions/AlreadyExistsException.java
+++ services/devices-service/src/main/java/ru/belkevglaz/ypa/exceptions/AlreadyExistsException.java
@@ -3,8 +3,6 @@ package ru.belkevglaz.ypa.exceptions;
 /**
  */
 public class AlreadyExistsException extends Exception {
-	public AlreadyExistsException() {
-	}
 
 	public AlreadyExistsException(String message) {
 		super(message);
--- services/devices-service/src/main/java/ru/belkevglaz/ypa/exceptions/NotExistsException.java
+++ services/devices-service/src/main/java/ru/belkevglaz/ypa/exceptions/NotExistsException.java
@@ -3,8 +3,6 @@ package ru.belkevglaz.ypa.exceptions;
 /**
  */
 public class NotExistsException extends Exception {
-	public NotExistsException() {
-	}
 
 	public NotExistsException(String message) {
 		super(message);
