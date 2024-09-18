5ca920e59560fb43cbb867bbe8e2fd489f6ab14c
Ivan Aksenov <belkevglaz@gmail.com>
2024-09-18 14:57:42 +0300

    Final workable compose with services

--- services/telemetry-service/src/main/java/ru/belkevglaz/ypa/kafka/KafkaProducer.java
+++ services/telemetry-service/src/main/java/ru/belkevglaz/ypa/kafka/KafkaProducer.java
@@ -2,13 +2,17 @@ package ru.belkevglaz.ypa.kafka;
 
 import io.smallrye.reactive.messaging.kafka.KafkaRecord;
 import jakarta.enterprise.context.ApplicationScoped;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import ru.belkevglaz.ypa.objects.Telemetry;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  *
  */
+@Slf4j
 @ApplicationScoped
 public class KafkaProducer {
 
@@ -19,7 +23,13 @@ public class KafkaProducer {
 
 		KafkaRecord<String, Telemetry> record = KafkaRecord.of(topic, null, telemetry);
 
-		emitter.send(record);
+		emitter.send(
+				record
+						.withAck(() -> {
+							log.info("📧 Sent telemetry to kafka topic [{}]: {}", topic, telemetry);
+							return CompletableFuture.completedFuture(null);
+						})
+		);
 
 		return telemetry;
 	}
